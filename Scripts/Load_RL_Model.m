cd41fb55325dd46ea34cd920a221c96653dd2933
ballsc <145720840+ballsc@users.noreply.github.com>
2026-02-22 17:01:22 -0600

    Updated startup files. Fixed issue with SAC_MPC and SAC_only models not compiling.

--- Scripts/Load_RL_Model.m
+++ Scripts/Load_RL_Model.m
@@ -1,6 +1,12 @@
 simStepSize = .05;
-
 initialVehicleVelocity = 8.9;
 
-load("busDefinitionsForRRSim.mat")
-open(".\RL_Environments\SAC_MPC_angle.slx")
+load("RL_BusDefinitions.mat")
+
+%% Uncomment if using the main SAC MPC model
+% open(".\RL_Environments\SAC_MPC_angle.slx")
+% load(".\Agents\SAC_MPC_agent.mat")
+
+%% Uncomment if using just SAC model
+open(".\RL_Environments\SAC_only_angle.slx")
+load(".\Agents\SAC_solo_agent.mat")
--- Scripts/RL_Script.m
+++ Scripts/RL_Script.m
@@ -1,7 +1,2 @@
 % Simulink RL model
-mdl = "RL_Model";
-agentblk = mdl + "/RLModel/RL Agent";
-load(".\Agents\base_agent.mat")
-load(".\Agents\RL_best_agent.mat")
-
-env = rlSimulinkEnv(mdl,agentblk,obsInfo,actInfo);
+%%this does nothing right no
