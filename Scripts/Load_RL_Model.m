b3a7283e82e7ed18b74a2753cfbb7631d9ed331a
ballsc <145720840+ballsc@users.noreply.github.com>
2026-02-22 13:16:57 -0600

    Added current files

---
+++ Scripts/Load_RL_Model.m
@@ -0,0 +1,6 @@
+simStepSize = .05;
+
+initialVehicleVelocity = 8.9;
+
+load("C:\Users\balls\Desktop\EcoCAR\git\CAV - 2025b\busDefinitionsForRRSim.mat")
+open("C:\Users\balls\Desktop\EcoCAR\git\CAV - 2025b\RL_Model.slx")
