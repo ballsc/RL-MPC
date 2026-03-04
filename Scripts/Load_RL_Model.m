c6a212726452d07d44865eae007467b7336e9ba3
ballsc <145720840+ballsc@users.noreply.github.com>
2026-03-04 13:03:30 -0600

    Trained agent for RL-MPC. Continuing to evaluation on first trained agent.

--- Scripts/Load_RL_Model.m
+++ Scripts/Load_RL_Model.m
@@ -4,9 +4,9 @@ initialVehicleVelocity = 8.9;
 load("RL_BusDefinitions.mat")
 
 %% Uncomment if using the main SAC MPC model
-% open(".\RL_Environments\SAC_MPC_angle.slx")
-% load(".\Agents\SAC_MPC_agent.mat")
+open(".\RL_Environments\SAC_MPC_angle.slx")
+load(".\Agents\SAC_MPC_agent.mat")
 
 %% Uncomment if using just SAC model
-open(".\RL_Environments\SAC_only_angle.slx")
-load(".\Agents\SAC_solo_agent.mat")
+% open(".\RL_Environments\SAC_only_angle.slx")
+% load(".\Agents\SAC_solo_agent.mat")
