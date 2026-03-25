31f5d1da42f69ebb9c0dab2b06be436a2d4c97bb
ballsc <145720840+ballsc@users.noreply.github.com>
2026-03-25 13:04:29 -0500

    Added evaluation oval track.

--- Scripts/Load_RL_Model.m
+++ Scripts/Load_RL_Model.m
@@ -5,7 +5,7 @@ load("RL_BusDefinitions.mat")
 
 %% Uncomment if using the main SAC MPC model
 open(".\RL_Environments\SAC_MPC_angle.slx")
-load(".\Agents\SAC_MPC_agent.mat")
+load(".\Agents\SAC_MPC_agent_Sang.mat")
 
 %% Uncomment if using just SAC model
 % open(".\RL_Environments\SAC_only_angle.slx")
