4f17f4313a71ffee599f2de694f3afdb4ff254a3
scb548 <scb548@cavs-19466-pc.hpc.msstate.edu>
2026-03-27 17:14:13 -0500

    Added two scripts for visualizing results. Added result images to results.

---
+++ Scripts/Visualization/mil_graphs_vts.m
@@ -0,0 +1,85 @@
+%% Function to find and plot the VTS targets of LCC
+
+%% TODO: add data from RL test 2
+
+% Gather Data from output
+data_mpc = load("Results\MPC_Best_Eval.mat");
+data_rl1 = load("Results\MPC_RL_Best_Eval.mat");
+
+data_mpc = data_mpc.data;
+data_rl1 = data_rl1.data;
+
+% data from MPC
+lateral_jerk_mpc = squeeze(find(data_mpc, "lateralJerk").Values.Data);
+swAngleDeg_mpc = find(data_mpc,"swAngleDeg").Values.Data;
+midOffset_mpc = find(data_mpc, "offset_diff").Values.Data;
+time = find(data_mpc, "lateralJerk").Values.Time;
+
+% data from RL test 1
+lateral_jerk_rl1 = squeeze(find(data_rl1, "lateralJerk").Values.Data);
+swAngleDeg_rl1 = find(data_rl1,"swAngleDeg").Values.Data;
+midOffset_rl1 = find(data_rl1, "midOffset").Values.Data;
+
+% data from RL test 2
+% lateral_jerk_rl2 = find(data_rl2, "lateralJerk").Values.Data;
+% swAngleDeg_rl2 = find(data_rl2,"swAngleDeg").Values.Data;
+% midOffset_rl2 = find(data_rl2, "midOffset").Values.Data;
+
+% Process data to get general VTS information
+max_lat_jerk_mpc = max(lateral_jerk_mpc);
+rms_lat_deviation_mpc = rms(midOffset_mpc);
+steering_busyness_mpc = sum(abs(diff(swAngleDeg_mpc)))/((time*20)/60);
+steering_busyness_mpc = steering_busyness_mpc(end);
+
+max_lat_jerk_rl1 = max(lateral_jerk_rl1);
+rms_lat_deviation_rl1 = rms(midOffset_rl1);
+steering_busyness_rl1 = sum(abs(diff(swAngleDeg_rl1)))/((time*20)/60);
+steering_busyness_rl1 = steering_busyness_rl1(end);
+
+%% Setup tiled plots
+tiledlayout(3,1,'TileSpacing', 'tight')
+ax1 = nexttile;
+
+% Plot steering wheel degrees
+plot(time,swAngleDeg_rl1, 'Color', '#642F6C', "DisplayName", "SAC+MPC", 'LineWidth',2)
+hold on
+plot(time,swAngleDeg_mpc, "r", "DisplayName","MPC", 'LineWidth',2)
+% plot(time,swAngleDeg_10, "Color", "#00A3AD", "DisplayName","S_a_n_g = 10", 'LineWidth',2)
+
+ylabel("Steering Wheel Angle (degrees)","FontName","Times New Roman", FontSize=16)
+
+legend(FontSize=16)
+grid on
+hold off
+
+% Plot Lateral Jerk
+ax2 = nexttile;
+
+plot(time,lateral_jerk_rl1,'Color', '#642F6C', "DisplayName","SAC+MPC", 'LineWidth',2)
+hold on
+plot(time,lateral_jerk_mpc,"r", "DisplayName","MPC", 'LineWidth',2)
+%plot(time,lateral_jerk_10,"Color", "#00A3AD", "DisplayName","S_a_n_g = 10", 'LineWidth',2)
+
+ylabel("Lateral Jerk (m/s^3)","FontName","Times New Roman", FontSize=16)
+grid on
+hold off
+
+% Plot Distance to Center Line
+ax3 = nexttile;
+
+set(gca, 'FontSize', 20)
+
+plot(time, midOffset_mpc, 'r', "DisplayName","S_a_n_g = 50", 'LineWidth',2)
+hold on
+plot(time, midOffset_rl1, "Color", '#642F6C', "DisplayName","S_a_n_g = 25", 'LineWidth',2)
+% plot(time,offset_error_10,"Color", "#00A3AD", "DisplayName","S_a_n_g = 10", 'LineWidth',2)
+
+ylabel("Offset From Center Line (m)","FontName","Times New Roman", FontSize=16)
+box on
+grid on
+hold off
+
+% Setup X axis
+linkaxes([ax1, ax2, ax3], 'x');
+xlim(ax1,[0 400])
+xlabel(ax3,"Time (s)","FontName","Times New Roman", FontSize=16)
---
+++ Scripts/Visualization/rl_training_results.m
@@ -0,0 +1,146 @@
+%% Function to find and plot the VTS targets of LCC
+
+%% TODO: add data from RL training with all outputs
+
+% Gather Data from output
+load("Results\Second_Training_Results.mat");
+
+data_straight = trainStats1_straight;
+data_right = trainStats2_right;
+% data_all = something;
+
+clearvars -except data_straight data_right data_left
+
+% data from Straight
+steps_s = data_straight.AverageSteps;
+reward_s = data_straight.AverageReward;
+q_s = data_straight.EpisodeQ0;
+total_s = data_straight.EpisodeIndex;
+
+% data from Right
+steps_r = data_right.AverageSteps;
+reward_r = data_right.AverageReward;
+q_r = data_right.EpisodeQ0;
+total_r = data_right.EpisodeIndex;
+
+% data from RL all
+% steps_a = data_all.AverageSteps;
+% reward_a = data_all.AverageReward;
+% q_a = data_all.EpisodeQ0;
+% total_a = max(data_all.EpisodeIndex);
+
+
+%% Setup tiled plot for Straight and Right
+tiledlayout(3,2,'TileSpacing', 'tight')
+ax1 = nexttile;
+
+% Plot Agent Steps Straight
+plot(total_s, steps_s, 'r', "DisplayName", "Straight", 'LineWidth',2)
+hold on
+
+xlim([0 max(total_s)])
+ylabel("Agent Steps","FontName","Times New Roman", FontSize=16)
+
+legend(FontSize=16)
+grid on
+hold off
+
+% Plot Agent Steps Right
+ax4 = nexttile;
+
+plot(total_r, steps_r, 'Color', '#642F6C', "DisplayName", "Right", 'LineWidth',2)
+hold on
+xlim([0 max(total_r)])
+%ylabel("Agent Steps","FontName","Times New Roman", FontSize=16)
+
+legend(FontSize=16)
+grid on
+hold off
+
+% Plot Total Reward Straight
+ax2 = nexttile;
+
+plot(total_s, reward_s, 'r', "DisplayName","Straight", 'LineWidth',2)
+hold on
+xlim([0 max(total_s)])
+ylabel("Total Reward","FontName","Times New Roman", FontSize=16)
+grid on
+hold off
+
+% Plot Total Reward Right
+ax5 = nexttile;
+
+plot(total_r, reward_r, 'Color', '#642F6C', "DisplayName","Right", 'LineWidth',2)
+hold on
+xlim([0 max(total_r)])
+%ylabel("Total Reward","FontName","Times New Roman", FontSize=16)
+grid on
+hold off
+
+% Plot Critic Q Value Straight
+ax3 = nexttile;
+
+set(gca, 'FontSize', 20)
+
+plot(total_s, q_s, 'r', "DisplayName","Straight", 'LineWidth',2)
+hold on
+xlim([0 max(total_s)])
+ylabel("Critic Q-Value","FontName","Times New Roman", FontSize=16)
+box on
+grid on
+hold off
+
+% Plot Critic Q Value Right
+ax6 = nexttile;
+
+set(gca, 'FontSize', 20)
+
+plot(total_r, q_r, 'Color', '#642F6C', "DisplayName", "Right", 'LineWidth',2)
+hold on
+xlim([0 max(total_r)])
+%ylabel("Critic Q-Value","FontName","Times New Roman", FontSize=16)
+box on
+grid on
+hold off
+
+
+%% Setup tiled plot for All
+% tiledlayout(3,1,'TileSpacing', 'tight')
+% ax1 = nexttile;
+% 
+% % Plot Agent Steps
+% plot(total_s, steps_s, 'r', "DisplayName", "all", 'LineWidth',2)
+% hold on
+% 
+% ylabel("Agent Steps","FontName","Times New Roman", FontSize=16)
+% 
+% legend(FontSize=16)
+% grid on
+% hold off
+% 
+% % Plot Total Reward
+% ax2 = nexttile;
+% 
+% plot(total_s, reward_s,'r', "DisplayName", "all", 'LineWidth',2)
+% hold on
+% 
+% ylabel("Total Reward","FontName","Times New Roman", FontSize=16)
+% grid on
+% hold off
+% 
+% % Plot Critic Q Value
+% ax3 = nexttile;
+% 
+% set(gca, 'FontSize', 20)
+% 
+% plot(total_s, q_s, 'r', "DisplayName", "all", 'LineWidth',2)
+% hold on
+% 
+% ylabel("Critic Q-Value","FontName","Times New Roman", FontSize=16)
+% box on
+% grid on
+% hold off
+% 
+% % Setup X axis
+% linkaxes([ax1, ax2, ax3], 'x');
+% xlabel(ax3,"Episode","FontName","Times New Roman", FontSize=16)
